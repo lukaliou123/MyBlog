23a39e4ae04dbacdac8f43da4189de507c8c1fd0
lucacio123 <jche9308@uni.sydney.edu.au>
2022-03-17 20:59:06 +0800

    更新写文章，显示文章分类

--- blog-api/src/main/java/com/example/blogapi/service/CategoryService.java
+++ blog-api/src/main/java/com/example/blogapi/service/CategoryService.java
@@ -1,7 +1,10 @@
 package com.example.blogapi.service;
 
 import com.example.blogapi.vo.CategoryVo;
+import com.example.blogapi.vo.Result;
 
 public interface CategoryService {
     CategoryVo findCategoryById(Long categoryId);
+
+    Result findAll();
 }
--- blog-api/src/main/java/com/example/blogapi/service/impl/CategoryServiceImpl.java
+++ blog-api/src/main/java/com/example/blogapi/service/impl/CategoryServiceImpl.java
@@ -1,13 +1,18 @@
 package com.example.blogapi.service.impl;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.example.blogapi.dao.mapper.CategoryMapper;
 import com.example.blogapi.dao.pojo.Category;
 import com.example.blogapi.service.CategoryService;
 import com.example.blogapi.vo.CategoryVo;
+import com.example.blogapi.vo.Result;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
@@ -20,5 +25,27 @@ public class CategoryServiceImpl implements CategoryService {
         CategoryVo categoryVo = new CategoryVo();
         BeanUtils.copyProperties(category,categoryVo);
         return categoryVo;
+    }
+
+    @Override
+    public Result findAll() {
+        List<Category> categories = categoryMapper.selectList(new LambdaQueryWrapper<>());
+        //页面交互的对象
+
+        return Result.success(copyList(categories));
+    }
+
+    public CategoryVo copy(Category category){
+        CategoryVo categoryVo = new CategoryVo();
+        BeanUtils.copyProperties(category,categoryVo);
+        categoryVo.setId(String.valueOf(category.getId()));
+        return categoryVo;
+    }
+    public List<CategoryVo> copyList(List<Category> categoryList){
+        List<CategoryVo> categoryVoList = new ArrayList<>();
+        for (Category category : categoryList) {
+            categoryVoList.add(copy(category));
+        }
+        return categoryVoList;
     }
 }
