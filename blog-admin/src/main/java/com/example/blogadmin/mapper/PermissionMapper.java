b9ffcb988046a904ea423c859e07d1fba7e974ec
lucacio123 <jche9308@uni.sydney.edu.au>
2022-03-20 17:16:41 +0800

    更新后台管理，权限表增删改查

---
+++ blog-admin/src/main/java/com/example/blogadmin/mapper/PermissionMapper.java
@@ -0,0 +1,7 @@
+package com.example.blogadmin.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.blogadmin.pojo.Permission;
+
+public interface PermissionMapper extends BaseMapper<Permission> {
+}
